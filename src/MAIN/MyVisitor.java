0178f9f9ef5bb24a49b7e8b84a77be2113fdd431
Cristian Suazo Jara <cristian.suazo.j@mail.pucv.cl>
2023-06-27 16:58:21 -0400

    Trabajo en operaciones y funciones matemáticas

--- src/MAIN/MyVisitor.java
+++ src/MAIN/MyVisitor.java
@@ -8,6 +8,8 @@ import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 import ANTLR.LexerT;
 import ANTLR.ParserTBaseVisitor;
 import ANTLR.ParserTParser;
+import ANTLR.ParserTParser.OperacionContext;
+import ANTLR.ParserTParser.FuncionContext;
 
 public class MyVisitor extends ParserTBaseVisitor<Integer> {
 
@@ -19,5 +21,35 @@ public class MyVisitor extends ParserTBaseVisitor<Integer> {
 		RuleContext nodoTerminal = (RuleContext) nodo;
 		String nombre = nodoTerminal.getClass().getSimpleName();
 		return nombre.substring(0, nombre.length() - 7);
+	}
+
+	Map<String, String> variables = new HashMap<String, String>();
+
+	public Integer visitSuma(ParserTParser.SumaContext ctx){
+		return 0;
+	}
+
+	public Integer visitResta(ParserTParser.RestaContext ctx){
+		return 0;
+	}
+
+	public Integer visitMultiplicacion(ParserTParser.MultiplicacionContext ctx){
+		return 0;
+	}
+
+	public Integer visitDivision(ParserTParser.DivisionContext ctx){
+		return 0;
+	}
+
+	public Integer visitExp(ParserTParser.ExpContext ctx){
+		return 0;
+	}
+
+	public Integer visitCos(ParserTParser.CosContext ctx){
+		return 0;
+	}
+
+	public Integer visitSqrt(ParserTParser.SqrtContext ctx){
+		return 0;
 	}
 }
